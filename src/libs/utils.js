98d2f51a7411db08ec5bfb4f721850e4b4fdb2a7
LK Nil <lk.nil@outlook.com>
2024-07-28 16:41:03 +0800

    feat: 更新style同步算法

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -17,8 +17,9 @@ export const insertStyle = (href) => {
  * @returns 
  */
 function isStyleTarget(targets) {
-  let list = Array.isArray(targets) ? targets : [targets];
-  
+  let list = targets.toString() === '[object NodeList]' ? targets : [targets];
+
+  window.a = targets;
   for (let i = 0; i < list.length; i ++) {
     if (['STYLE', 'LINK'].indexOf(list[i].nodeName) !== -1) {
       return true;
@@ -32,9 +33,55 @@ function isStyleTarget(targets) {
  * @param {Node} root 
  */
 function syncStyle(root) {
-  const styles = document.querySelectorAll('link, style');
-  root.innerHTML = '';
-  styles.forEach(item => root.appendChild(item.cloneNode(true)));
+  const mainStyles = document.querySelectorAll('link, style');
+  const shadowStyles = root.querySelectorAll('link, style');
+  let si = -1;
+  let cursor = shadowStyles[si + 1] ? shadowStyles[si + 1] : null;
+
+  mainStyles.forEach(item => {
+    const node = item.cloneNode(true);
+    let val = '';
+    let i = 0;
+
+    while ( i < node.attributes.length) {
+      val += node.attributes[i].nodeValue;
+      i++;
+    }
+
+    if (node.nodeName === 'STYLE') {
+      val += node.textContent;
+    }
+    let hash = time33(val);
+
+    // 尝试寻找原来已经存在的节点
+    let tmpi = si + 1;
+    while (tmpi < shadowStyles.length) {
+      if (hash.toString() === shadowStyles[tmpi].getAttribute('data-hash')) {
+        for (let j = si + 1; j < tmpi; j ++) {
+          shadowStyles[j].remove();
+        }
+        si = tmpi;
+        cursor = shadowStyles[si + 1] ? shadowStyles[si + 1] : null;
+        break;
+      }
+      tmpi ++;
+    }
+    if (si === tmpi) { // 节点已存在
+      // 不处理
+    } else {
+      node.setAttribute('data-hash', hash);
+      if (cursor === null) {
+        root.appendChild(node);
+      } else {
+        root.insertBefore(node, cursor);
+      }
+    }
+  });
+
+  // remove
+  for (let i = si + 1; i < shadowStyles.length; i ++) {
+    shadowStyles[i].remove();
+  }
 }
 
 /**
@@ -51,6 +98,7 @@ export const createShadowRoot = (host, entry) => {
   const styleDiv = document.createElement('div');
   styleDiv.id = '__style__';
   shadowRoot.appendChild(styleDiv);
+  window.styleDiv = styleDiv;
 
   // 需要在沙箱运行
   // 沙箱里面定制化document行为
@@ -99,4 +147,15 @@ export const getShadowDocument = (shadowRoot, originalDocument) => {
       return shadowRoot[key].bind(shadowRoot);
     }
   });
+}
+
+export const time33 = (text) => {
+  let hash = 5381;
+  let i = text.length;
+
+  while (i) {
+    hash = (hash * 33) ^ text.charCodeAt(--i);
+  }
+
+  return hash >>> 0;
 }
--- src/scenes/2/main.js
+++ src/scenes/2/main.js
@@ -7,7 +7,8 @@ createShadowRoot(shadowRoot, '/scenes/2/sub.js');
 setTimeout(() => {
     const mainRoot = document.getElementById('main-root');
     const { Button } = SemiUI;
-    const commonStyle = document.getElementById('common-style');
+    let commonStyle = document.getElementById('common-style');
+    let originalStyle = document.querySelectorAll('link, style');
 
     function changeTextStyle(color) {
         commonStyle.innerHTML = `
@@ -15,6 +16,51 @@ setTimeout(() => {
             color: ${color};
         }
         `;
+    }
+
+    function getRandomStyle() {
+        const style = document.createElement('style');
+        style.innerHTML = `
+       .common-text {
+            color: ${['black', 'red', 'green', 'blue'][Math.floor(Math.random()*4)]};
+        }
+        `;
+        return style;
+    }
+
+    function appendTextStyle() {
+        const style = getRandomStyle();
+        document.querySelector('head').appendChild(style);
+    }
+
+    function insertBeforeTextStyle() {
+        const style = getRandomStyle();
+        if (originalStyle[0]) {
+            document.querySelector('head').insertBefore(style, originalStyle[0]);
+
+        } else {
+            document.querySelector('head').appendChild(style);
+        }
+    }
+
+    function resetTextStyle() {
+        document.querySelectorAll('link, style').forEach(item => {
+            item.remove();
+        });
+        const head = document.querySelector('head');
+        originalStyle.forEach(item => {
+            head.appendChild(item.cloneNode(true));
+        });
+
+        originalStyle = document.querySelectorAll('link, style');
+        commonStyle = document.getElementById('common-style');
+    }
+
+    function randomInsertTextStyle() {
+        const style = getRandomStyle();
+        const head = document.querySelector('head');
+        const index = Math.floor(Math.random() * originalStyle.length);
+        head.insertBefore(style, originalStyle[index]);
     }
 
     class App extends React.Component {
@@ -30,6 +76,10 @@ setTimeout(() => {
                 React.createElement(Button, { onClick: () => changeTextStyle('red') }, 'red'),
                 React.createElement(Button, { onClick: () => changeTextStyle('green') }, 'green'),
                 React.createElement(Button, { onClick: () => changeTextStyle('blue') }, 'blue'),
+                React.createElement(Button, { onClick: () => appendTextStyle('blue') }, 'append style'),
+                React.createElement(Button, { onClick: () => insertBeforeTextStyle('blue') }, 'insert style before'),
+                React.createElement(Button, { onClick: () => randomInsertTextStyle('blue') }, 'random insert'),
+                React.createElement(Button, { onClick: () => resetTextStyle('blue') }, 'reset style'),
                 React.createElement('p', { className: 'common-text' }, 'text outside shadow-root'),
             );
         }
